d0e04720625894bb9cdf2f38b639d1bb5c03141a
james-dibble <james@jdibble.co.uk>
2013-11-21 15:13:09 +0000

    What the hell GIT??

---
+++ EmbeddedSystems.Services/EmbeddedSystems.Services.HandsetApi/App_Start/ApiFilterConfig.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ApiFilterConfig.cs" company="ESD">
+//    Copyright 2013
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace EmbeddedSystems.Services.HandsetApi
+{
+    using System.Web.Http;
+
+    using EmbeddedSystems.Security;
+
+    public class ApiFilterConfig
+    {
+        public static void RegisterWebApiFilters(System.Web.Http.Filters.HttpFilterCollection filters)
+        {
+            var authorizationService =
+                (IAuthorizationService)GlobalConfiguration.Configuration.DependencyResolver.GetService(typeof(IAuthorizationService));
+
+            filters.Add(new RequiresAuthenticationFilter(authorizationService));
+        } 
+    }
+}
