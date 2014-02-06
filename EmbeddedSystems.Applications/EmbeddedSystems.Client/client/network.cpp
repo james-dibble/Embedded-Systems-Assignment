c698b0ae0611e78022b082a39130391311bff9bf
gmasters <mastergmasters@gmail.com>
2014-02-06 17:11:13 +0000

    works properly now

--- EmbeddedSystems.Applications/EmbeddedSystems.Client/client/network.cpp
+++ EmbeddedSystems.Applications/EmbeddedSystems.Client/client/network.cpp
@@ -30,35 +30,31 @@ QByteArray Network::getRequest(QUrl url)
     QByteArray response;
 
     QNetworkRequest request(url);
-
     request.setUrl(url);
-//    request.setHeader( QNetworkRequest::ContentTypeHeader, "some/type" );
 
     request.setRawHeader("User-Agent", "PINTS");
 
+    qDebug() << deviceName << pin;
 
+ //   QString concatenated = QString("%1:%2").arg(deviceName,pin);
+    QString namePinKey;
+    namePinKey = QString("%1:%2").arg(deviceName).arg(pin);
+    qDebug() << "namePinKey: " << namePinKey;
 
-    QString concatenated = deviceName + ":" + pin;
  //   QByteArray data = concatenated.toLocal8Bit().toBase64();
     QByteArray data ;
-    data.append( concatenated);
-    data.toBase64();
-    qDebug() << data.toBase64();
-  //  QString::
-  //  QString headerData = "Basic " + data.toBase64();
-    QString headerData = (QString) "Basic " + "MTIzOjEyMzQ=";
-  //  request.setRawHeader("Authorization", headerData.toLocal8Bit());
-    request.setRawHeader("Authorization", headerData.toUtf8());
+    data.append(namePinKey);
 
-    qDebug() << headerData;
+    qDebug() << "\nIt should be: MTIzOjEyMzQ=\n";
+    qDebug() << "data base64: " << data.toBase64();
+
+    QString headerData = "Basic " + data.toBase64();
+ //   QString headerData = (QString) "Basic " + "MTIzOjEyMzQ=";
+
+    request.setRawHeader("Authorization", headerData.toLocal8Bit());
 
     QList<QByteArray> list = request.rawHeaderList();
     qDebug() << list;
-
-    for (int i=0; i < list.count();i++)
-    {
-        qDebug() << list.at(i).data();
-    }
 
     networkMan->get(request);
 
@@ -78,6 +74,8 @@ QByteArray Network::replyReceived(QNetworkReply* reply)
 {
     QByteArray replyData;
     qDebug() << "reply received";
+    qDebug() << "Qt NetworkCode Error" << reply->error();
+    qDebug() << "Actual HTTP response" << reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toUInt();
 
     if (reply->error() == QNetworkReply::NoError)
     {
@@ -87,25 +85,9 @@ QByteArray Network::replyReceived(QNetworkReply* reply)
     }
     else
     {
-      //  qDebug() << "the HTTP Status Code" <
-            qDebug() << "the error" << reply->error();
+
+    //    qDebug() << "the error" << reply->attribute();
             replyData = reply->readAll();
-
-
-            QVariant statusCode = reply->attribute( QNetworkRequest::HttpStatusCodeAttribute );
-                if ( !statusCode.isValid() )
-                    return replyData;
-
-                int status = statusCode.toInt();
-
-                if ( status != 200 )
-                {
-                    QString reason = reply->attribute( QNetworkRequest::HttpReasonPhraseAttribute ).toString();
-                    qDebug() << reason;
-                }
-
-
-
 
             QString str = QString::fromUtf8(replyData.data(), replyData.size());
             qDebug() << "the reply " << str;
